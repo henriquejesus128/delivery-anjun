c717517cd2d64722385138d5447adcd19c7e0ade
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-06 01:28:33 -0300

    feat: prisma repository users

---
+++ src/modules/users/repositories/prisma/users.prisma.repository.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { UsersRepository } from '../users.repository';
+import { CreateUserDto } from '../../dto/create-user.dto';
+import { UpdateUserDto } from '../../dto/update-user.dto';
+import { User } from '../../entities/user.entity';
+
+@Injectable()
+export class UsersPrismaRepository implements UsersRepository {
+  create(data: CreateUserDto): User | Promise<User> {
+    throw new Error('Method not implemented.');
+  }
+  findAll(): User[] | Promise<User[]> {
+    throw new Error('Method not implemented.');
+  }
+  findOne(id: string): User | Promise<User> {
+    throw new Error('Method not implemented.');
+  }
+  update(id: string, data: UpdateUserDto): User | Promise<User> {
+    throw new Error('Method not implemented.');
+  }
+  delete(id: string): void | Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  findByEmail(email: string): User | Promise<User> {
+    throw new Error('Method not implemented.');
+  }
+}
