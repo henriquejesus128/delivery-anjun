058be6b42f0e5a33ec3e8e4c511e5f1dd6f64c82
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-06 16:24:37 -0300

    feat: jwt strategy

---
+++ src/modules/auth/jwt.strategy.ts
@@ -0,0 +1,17 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { strict } from 'assert';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.SECRET_KEY,
+    });
+  }
+
+  async validate(email: string) {
+    return { email: email };
+  }
+}
