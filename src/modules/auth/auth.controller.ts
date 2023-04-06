cbb15cd6082c834f138529a0ab91f7f9b732cee4
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-06 16:08:06 -0300

    fix: controller auth

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,10 +1,14 @@
-import { Body, Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { IAuthLogin } from './auth.interface';
+import { LocalAuthGard } from './local-auth.guard';
 
 @Controller('login')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  @UseGuards(LocalAuthGard)
   async login(@Body() user: IAuthLogin) {
     return this.authService.login(user.email);
   }
