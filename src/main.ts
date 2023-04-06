b1f76b3559ae13fe0feecd5dbd4e509721df5d8c
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-06 16:51:20 -0300

    fix: main swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Project Delivery')
+    .setDescription(
+      'A crud of users and products where to deliver these products to other users',
+    )
+    .setVersion('1,0');
+
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true }),
     new ValidationPipe({
