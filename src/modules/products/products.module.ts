3f56a9f45b31872f6e56fcfa9469d9225efb209b
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-06 12:49:15 -0300

    fix: module products

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { PrismaService } from 'src/database/prisma.service';
+import { ProductsPrismaRepository } from './repositories/prisma/products.prisma.repository';
+import { ProductsRepository } from './repositories/products.repository';
 
 @Module({
   controllers: [ProductsController],
-  providers: [ProductsService]
+  providers: [
+    ProductsService,
+    PrismaService,
+    {
+      provide: ProductsRepository,
+      useClass: ProductsPrismaRepository,
+    },
+  ],
 })
 export class ProductsModule {}
