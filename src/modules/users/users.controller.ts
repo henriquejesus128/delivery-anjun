6ae95f02189533e48839fcf06cde4edea50253d7
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-06 16:30:10 -0300

    fix: controller users

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -9,10 +9,12 @@ import {
   ParseUUIDPipe,
   UseInterceptors,
   ClassSerializerInterceptor,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtAuthGard } from '../auth/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -31,18 +33,21 @@ export class UsersController {
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGard)
   @UseInterceptors(ClassSerializerInterceptor)
   findOne(@Param('id', ParseUUIDPipe) id: string) {
     return this.usersService.findOne(id);
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGard)
   @UseInterceptors(ClassSerializerInterceptor)
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGard)
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
   }
