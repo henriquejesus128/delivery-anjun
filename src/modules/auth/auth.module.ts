fc9e6206ca582f5c871334ce6395fb963e094394
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-06 16:02:50 -0300

    fix: module auth

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UsersService } from '../users/users.service';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
-    UsersService,
+    UsersModule,
     PassportModule,
     JwtModule.register({
       secret: process.env.SECRET_KEY,
