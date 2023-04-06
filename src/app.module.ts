c4a10e28f00835001ad39b79f0940535b159a12e
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-06 20:24:28 -0300

    feat: recipient

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { UsersModule } from './modules/users/users.module';
 import { ProductsModule } from './modules/products/products.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { AdressesModule } from './modules/adresses/adresses.module';
+import { RecipientsModule } from './modules/recipients/recipients.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule, AuthModule, AdressesModule],
+  imports: [UsersModule, ProductsModule, AuthModule, AdressesModule, RecipientsModule],
   controllers: [],
   providers: [],
 })
