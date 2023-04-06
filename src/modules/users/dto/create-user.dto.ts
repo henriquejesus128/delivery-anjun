39158e852fa34ed1f26d7a43e6d80bc0cd509653
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-06 00:28:48 -0300

    fix: dto user create

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,5 +1,21 @@
+import { Transform } from 'class-transformer';
+import { IsString, IsNotEmpty, IsEmail, MinLength } from 'class-validator';
+import { hashSync } from 'bcryptjs';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @Transform(({ value }: { value: string }) => hashSync(value, 10), {
+    groups: ['transform'],
+  })
   password: string;
 }
