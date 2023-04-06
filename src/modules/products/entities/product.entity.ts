265f67a4acb9553f73371d9dcf3b7b17c850ec52
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-06 12:48:58 -0300

    fix: entity Product

--- src/modules/products/entities/product.entity.ts
+++ src/modules/products/entities/product.entity.ts
@@ -1,1 +1,11 @@
-export class Product {}
+import { randomUUID } from 'node:crypto';
+
+export class Product {
+  readonly id: string;
+  name: string;
+  type: string;
+
+  constructor() {
+    this.id = randomUUID();
+  }
+}
