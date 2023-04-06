c427603bfee69eae575e8fec256c912179eda2e9
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-06 14:38:08 -0300

    fix: service auth

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+  async validatedUser(email: string, password: string) {
+    const user = await this.userService;
+  }
+}
