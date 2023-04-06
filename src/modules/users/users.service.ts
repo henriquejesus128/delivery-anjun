b293f41113aece3fee02364f6829ed56d8a3fea3
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-06 01:23:39 -0300

    fix: users services

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -2,7 +2,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { User } from './entities/user.entity';
 import { UsersRepository } from './repositories/users.repository';
 
 @Injectable()
