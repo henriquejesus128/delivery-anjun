81c0d164896b4d7f7c73268f1b31149295e418d5
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-06 16:01:12 -0300

    fix: controller auth to login

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { IAuthLogin } from './auth.interface';
 
-@Controller('auth')
+@Controller('login')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
   async login(@Body() user: IAuthLogin) {
