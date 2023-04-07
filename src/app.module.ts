3b5117db2607366785ebfd746a9c9c2c7ab88650
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-07 17:56:30 -0300

    fix: code clear

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
-import { CustomersModule } from './modules/customers/customers.module';
 import { ProductsModule } from './modules/products/products.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, CustomersModule, ProductsModule],
+  imports: [UsersModule, AuthModule, ProductsModule],
   controllers: [],
   providers: [],
 })
