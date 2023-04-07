92ab85a20cd695b3f8bd29496dba5a6969a2e7ed
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-07 17:56:06 -0300

    feat: products

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { CustomersModule } from './modules/customers/customers.module';
+import { ProductsModule } from './modules/products/products.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, CustomersModule],
+  imports: [UsersModule, AuthModule, CustomersModule, ProductsModule],
   controllers: [],
   providers: [],
 })
---
+++ src/modules/products/products.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+
+@Injectable()
+export class ProductsService {
+  create(createProductDto: CreateProductDto) {
+    return 'This action adds a new product';
+  }
+
+  findAll() {
+    return `This action returns all products`;
+  }
+
+  findOne(id: string) {
+    return `This action returns a #${id} product`;
+  }
+}
