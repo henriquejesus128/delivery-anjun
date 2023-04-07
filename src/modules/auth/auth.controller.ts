f665edaaefc99498c7885bb65d12e21ce1632eba
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-07 16:33:58 -0300

    fix: clear code

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { IAuthLogin } from './auth.interface';
-import { LocalAuthGuard } from './local-auth.guard';
+import { LocalAuthGuard } from './local/local-auth.guard';
 
 @Controller('login')
 export class AuthController {
