3f14824bfc350875b5350f3177d950a4d4766af2
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-07 17:22:25 -0300

    feat: customer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { CustomersModule } from './modules/customers/customers.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, CustomersModule],
   controllers: [],
   providers: [],
 })
