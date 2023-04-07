fc121fccec04d005f3e9e8f0bd72fdb27a8e07c5
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-06 23:25:23 -0300

    fix: recipient repository

--- src/modules/recipients/repositories/recipients.repository.ts
+++ src/modules/recipients/repositories/recipients.repository.ts
@@ -4,8 +4,6 @@ import { Recipient } from '../entities/recipient.entity';
 export abstract class RecipientsRepository {
   abstract create(data: CreateRecipientDto): Promise<Recipient> | Recipient;
   abstract findAll(): Promise<Recipient[]> | Recipient[];
-  abstract findOne(
-    data: CreateRecipientDto,
-  ): Promise<Recipient | undefined> | Recipient;
+  abstract findOne(id: string): Promise<Recipient | undefined> | Recipient;
   abstract findName(name: string): Promise<Recipient | undefined> | Recipient;
 }
