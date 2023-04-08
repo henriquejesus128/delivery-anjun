2d6091238e2e508e74af14070271a6d91a8bf03e
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-08 00:00:00 -0300

    fix: modules orders

--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Module({
   controllers: [OrdersController],
-  providers: [OrdersService]
+  providers: [OrdersService, PrismaService],
+  exports: [OrdersService],
 })
 export class OrdersModule {}
