26f5eccdb09caf06db9b7f26a6fad187e1b773da
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-08 02:48:42 -0300

    fix: update order dto

--- src/modules/orders/dto/update-order.dto.ts
+++ src/modules/orders/dto/update-order.dto.ts
@@ -1,4 +1,11 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateOrderDto } from './create-order.dto';
+import { Status } from '@prisma/client';
+import { IsEnum, IsNotEmpty } from 'class-validator';
 
-export class UpdateOrderDto extends PartialType(CreateOrderDto) {}
+export class UpdateOrderDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsEnum(Status)
+  status: Status;
+}
