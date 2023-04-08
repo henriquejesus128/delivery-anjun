a8e27d223c3d2a98b8356da448e88448d347b8f5
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-08 01:08:42 -0300

    fix: dto order create

--- src/modules/orders/dto/create-order.dto.ts
+++ src/modules/orders/dto/create-order.dto.ts
@@ -1,10 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
-import { CreateProductDto } from 'src/modules/products/dto/create-product.dto';
 
 export class CreateOrderDto {
   @ApiProperty()
   @IsString()
   @IsNotEmpty()
-  products: CreateProductDto[];
+  products: string[];
 }
