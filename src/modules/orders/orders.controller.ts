c8dbe4ad49410f18013fa9c2ad897834eda46f71
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-08 02:49:03 -0300

    fix: controller order

--- src/modules/orders/orders.controller.ts
+++ src/modules/orders/orders.controller.ts
@@ -8,6 +8,8 @@ import {
   Delete,
   Request,
   UseGuards,
+  UseInterceptors,
+  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
@@ -19,34 +21,51 @@ import { JwtAuthGuard } from '../auth/jwt/jwt-auth.guard';
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
-  @Post()
+  @Post(':id')
   @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
+  @UseInterceptors(ClassSerializerInterceptor)
   create(
     @Body() createOrderDto: CreateOrderDto,
     @Request() req: any,
     @Param('id') id_customer: string,
   ) {
-    const { subject } = req.user.data;
-    return this.ordersService.create(createOrderDto, subject, id_customer);
+    const { sub } = req.user.data;
+    return this.ordersService.create(createOrderDto, sub, id_customer);
   }
 
   @Get()
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
   findAll() {
     return this.ordersService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.ordersService.findOne(id);
+  @Get('sender/:id')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  findBySender(@Param('id') id: string) {
+    return this.ordersService.findBySender(id);
+  }
+
+  @Get('recipient/:id')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  findByRecipient(@Param('id') id: string) {
+    return this.ordersService.findByRecipient(id);
   }
 
   @Patch(':id')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(ClassSerializerInterceptor)
   update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
     return this.ordersService.update(id, updateOrderDto);
   }
 
   @Delete(':id')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
   remove(@Param('id') id: string) {
     return this.ordersService.remove(id);
   }
