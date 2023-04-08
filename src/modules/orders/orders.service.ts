64b6c42b04b022067bb26ddaad5c0d9eabd192d1
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-08 01:45:30 -0300

    fix: service order

--- src/modules/orders/orders.service.ts
+++ src/modules/orders/orders.service.ts
@@ -1,14 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { PrismaService } from 'src/database/prisma.service';
+import { CustomersService } from '../customers/customers.service';
+import { ProductsService } from '../products/products.service';
+import { Product } from '@prisma/client';
 
 @Injectable()
 export class OrdersService {
+  constructor(
+    private prisma: PrismaService,
+    private customersService: CustomersService,
+    private productsService: ProductsService,
+  ) {}
   async create(
     createOrderDto: CreateOrderDto,
-    email: string,
+    id_user: string,
     id_customer: string,
-  ) {}
+  ) {
+    const findCustomer = await this.customersService.findOne(id_customer);
+
+    if (!findCustomer) {
+      throw new ConflictException(`Customer already exists!`);
+    }
+
+    const products = createOrderDto.products;
+
+    let productList: Product[] = [];
+
+    for (let i = 0; i < products.length; i++) {
+      let productName = products[i];
+      const findProduct = await this.productsService.findByName(productName);
+      if (!findProduct) {
+        productList.push(
+          await this.productsService.create({ name: productName }),
+        );
+      } else {
+        productList.push(findProduct);
+      }
+    }
+
+    // const order = await this.prisma.order.create({
+    //   data: {
+    //     ...createOrderDto,
+    //     senderId: id_user,
+    //     recipientId: id_customer,
+    //   },
+    // });
+    // return order;
+  }
 
   async findAll() {
     return `This action returns all orders`;
