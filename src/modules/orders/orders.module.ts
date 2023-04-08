e6e4838942f84303467ce619ea761f64066eec0a
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-08 01:08:28 -0300

    fix: module order

--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -3,10 +3,11 @@ import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { PrismaService } from 'src/database/prisma.service';
 import { CustomersService } from '../customers/customers.service';
+import { ProductsService } from '../products/products.service';
 
 @Module({
   controllers: [OrdersController],
-  providers: [OrdersService, PrismaService, CustomersService],
+  providers: [OrdersService, PrismaService, CustomersService, ProductsService],
   exports: [OrdersService],
 })
 export class OrdersModule {}
