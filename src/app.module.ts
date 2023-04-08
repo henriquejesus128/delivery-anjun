18362b53e4d787859c8b113e5df241fa100ad90f
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-07 23:56:27 -0300

    feat: orders

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ProductsModule } from './modules/products/products.module';
 import { CustomersModule } from './modules/customers/customers.module';
+import { OrdersModule } from './modules/orders/orders.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, ProductsModule, CustomersModule],
+  imports: [UsersModule, AuthModule, ProductsModule, CustomersModule, OrdersModule],
   controllers: [],
   providers: [],
 })
