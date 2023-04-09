86f67419b943ad5e3be3adb2b1e597501c4687c8
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-09 01:48:03 -0300

    fix: order module

--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { PrismaService } from 'src/database/prisma.service';
-import { CustomersService } from '../customers/customers.service';
-import { ProductsService } from '../products/products.service';
+import { CustomersModule } from '../customers/customers.module';
+import { ProductsModule } from '../products/products.module';
 
 @Module({
   controllers: [OrdersController],
-  providers: [OrdersService, PrismaService, CustomersService, ProductsService],
+  providers: [OrdersService, PrismaService],
   exports: [OrdersService],
+  imports: [CustomersModule, ProductsModule],
 })
 export class OrdersModule {}
